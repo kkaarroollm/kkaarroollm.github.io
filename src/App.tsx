071db65a2b44df31360ba417b33eebea9d0664a9
kkaarroollm <mkarol.4514@gmail.com>
2025-03-04 12:31:52 +0100

    we recruiting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import weAreRecruiting from './assets/we_re_recruiting.png'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
           <div className="text-center">
               <h1 className="text-4xl font-bold">KAROL MARSZALEK PORTFOLIO</h1>
               <p className="text-lg text-gray-600 mt-2">Coming soon...</p>
+              <p className="flex items-center justify-center"><img src={weAreRecruiting} alt="We are hiring" className="w-64 h-32 items" /></p>
           </div>
       </>
   )
